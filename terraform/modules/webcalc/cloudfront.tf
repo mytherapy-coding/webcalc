17b503eb38f9249b59ea3219c9b84e8e9afb0dd3
Alena <102840105+mytherapy-coding@users.noreply.github.com>
2024-04-13 19:51:58 -0400

    added terraform

---
+++ terraform/modules/webcalc/cloudfront.tf
@@ -0,0 +1,77 @@
+resource "aws_cloudfront_distribution" "webcalc_distribution" {
+  enabled             = true
+  is_ipv6_enabled     = true
+  default_root_object = aws_s3_object.webcalc["index"].key
+  price_class         = "PriceClass_100"
+
+  origin {
+    domain_name              = aws_s3_bucket.webcalc_bucket.bucket_regional_domain_name
+    origin_id                = aws_s3_bucket.webcalc_bucket.id
+    origin_access_control_id = aws_cloudfront_origin_access_control.s3_oac.id
+  }
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = aws_s3_bucket.webcalc_bucket.id
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
+
+data "aws_iam_policy_document" "allow_access" {
+  statement {
+    sid       = "AllowCloudFrontServicePrincipal"
+    effect    = "Allow"
+    actions   = ["s3:GetObject"]
+    resources = ["${aws_s3_bucket.webcalc_bucket.arn}/*"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["cloudfront.amazonaws.com"]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "AWS:SourceArn"
+      values   = ["${aws_cloudfront_distribution.webcalc_distribution.arn}"]
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "allow_access" {
+  bucket = aws_s3_bucket.webcalc_bucket.id
+  policy = data.aws_iam_policy_document.allow_access.json
+}
+
+
+resource "aws_cloudfront_origin_access_control" "s3_oac" {
+  name                              = "example-s3-oac"
+  description                       = "AOC Policy"
+  origin_access_control_origin_type = "s3"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
+}
+
+
---
+++ terraform/modules/webcalc/static.tf
@@ -0,0 +1,64 @@
+# Define the AWS S3 bucket for hosting the static website
+resource "aws_s3_bucket" "webcalc_bucket" {
+  bucket = "static-webcalc-2" # Replace with your unique bucket name
+
+}
+
+resource "aws_s3_bucket_website_configuration" "webcalc" {
+  bucket = aws_s3_bucket.webcalc_bucket.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+}
+
+resource "aws_s3_bucket_versioning" "webcalc" {
+  bucket = aws_s3_bucket.webcalc_bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+
+resource "aws_s3_bucket_public_access_block" "webcalc" {
+  bucket = aws_s3_bucket.webcalc_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+
+
+locals {
+  files = {
+    index = {
+      name = "index.html"
+      type = "text/html"
+    },
+    style = {
+      name = "styles.css"
+      type = "text/css"
+    },
+    script = {
+      name = "script.js"
+      type = "application/javascript"
+    },
+  }
+  source_path = "../../../new_static/"
+}
+
+
+resource "aws_s3_object" "webcalc" {
+  for_each = local.files
+
+  bucket        = aws_s3_bucket.webcalc_bucket.id
+  key           = each.value.name
+  source        = "${local.source_path}/${each.value.name}" # Path to your local index.html file
+  content_type  = each.value.type
+  etag          = filemd5("${local.source_path}/${each.value.name}")
+  cache_control = "max-age=60"
+}
+
